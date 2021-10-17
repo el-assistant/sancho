2232ce31dc6dbdb8a094e09b5bcb894d09246be2
Renan Fonseca <renanfonseca@gmail.com>
2021-10-17 17:08:07 +0200

    Add logging

--- sancho/etl.py
+++ sancho/etl.py
@@ -1,5 +1,6 @@
 """ Contains function to download repos from github, and preprocess. ETL resources are located in PROJECT_ROOT/data/
 """
+from loguru import logger
 import dotenv, os
 
 dotenv.load_dotenv()
@@ -8,13 +9,17 @@ assert token is not None
 
 from github import Github
 from git import Repo
-
+import git
 
 g = Github(token)
 
 
 def clone_starred_python():
     for r in g.search_repositories(query="language:python", sort="stars")[:500]:
-        Repo.clone_from(
-            r.html_url, f"data/repos/{r.full_name}", multi_options=["--depth=1"]
-        )
+        logger.info(f"Cloning {r.full_name=}")
+        try:
+            Repo.clone_from(
+                r.html_url, f"data/repos/{r.full_name}", multi_options=["--depth 1"]
+            )
+        except git.exc.GitCommandError:
+            logger.info(f"Failed to fetch {r.html_url}")
