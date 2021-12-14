3e97aa42815de2568c61bb77fd3204d152fee6dc
Renan Fonseca <renanfonseca@gmail.com>
2021-12-14 20:36:49 -0300

    Update neo4j schema

--- sancho/neo4jschema.py
+++ sancho/neo4jschema.py
@@ -30,8 +30,24 @@ class ASTnode(nm.StructuredNode):
     end = nm.IntegerProperty()
     content = nm.StringProperty()
     order = nm.IntegerProperty()
-    children = nm.RelationshipTo("ASTnode", "CHILDREN")
-    next_ = nm.RelationshipTo("ASTnode", "NEXT")
+    children = nm.RelationshipTo("ASTnode", "D")
+    next_ = nm.RelationshipTo("ASTnode", "N")
+
+
+class FileNode(nm.StructuredNode):
+    """Defines schema for a file node in Neo4j db."""
+
+    path = nm.RelationshipFrom("DirNode", "D")
+    name = nm.StringProperty(required=True)
+    ast_root = nm.RelationshipTo("ASTnode", "D")
+
+
+class DirNode(nm.StructuredNode):
+    """Defines schema for a directory node in Neo4j db."""
+
+    path = nm.RelationshipFrom("DirNode", "D")
+    name = nm.StringProperty()
+    project_root = nm.BooleanProperty()
 
 
 nm.install_all_labels()
