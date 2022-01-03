b368d7da957c7533642bac5ee45ef536713b7a75
Renan Fonseca <renanfonseca@gmail.com>
2022-01-03 14:02:35 +0100

    [WIP] Implement LOAD CSV

--- sancho/etl.py
+++ sancho/etl.py
@@ -27,3 +27,47 @@ def clone_starred_python():
             )
         except git.exc.GitCommandError:
             logger.info(f"Failed to fetch {r.html_url}")
+
+
+def convert_parsed_text_to_csv(parsed: model.ParsedText) -> model.ASTnodesTable:
+    assert parsed.tree and parsed.text
+    raise NotImplementedError  # TODO: implement it
+
+
+def get_parsed_as_csv(path: str) -> model.ASTnodesTable:
+    from sancho.parsing import get_native_ast
+
+    return convert_parsed_text_to_csv(get_native_ast(path))
+
+
+def load_parsed(parsed: model.ASTnodesTable):
+    raise NotImplementedError  # TODO: implement it
+
+
+def load_files(table: model.FilesTable):
+    raise NotImplementedError  # TODO: implement it
+
+
+def connect_files_and_AST(table: model.FilesTable):
+    raise NotImplementedError  # TODO: implement it
+
+
+def load_repo(repo: model.Repo):
+    def make_files_table(repo: model.Repo) -> model.FilesTable:
+        raise NotImplementedError  # TODO: implement it
+
+    def is_code_file(f: model.FileRowFormat) -> bool:
+        from pathlib import Path
+
+        p = Path(f.full_path)
+        return p.suffix == ".py"
+
+    filestable = make_files_table(repo)
+
+    load_files(filestable)
+
+    for f in filestable.rows:
+        if is_code_file(f):
+            load_parsed(get_parsed_as_csv(f.full_path))
+
+    connect_files_and_AST(filestable)
